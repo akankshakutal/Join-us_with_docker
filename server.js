ccc18fce08018cda318e736d3f2fd3ba5531c440
akankshakutal <44019583+akankshakutal@users.noreply.github.com>
2019-04-21 10:51:24 +0530

    Create simple server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const port = process.env.PORT || 8000;
+const app = express();
+let count = 0;
+
+const insertData = function(req, res) {
+  res.send("" + count++);
+};
+
+const getData = function(req, res) {
+  res.send("" + count);
+};
+
+app.use(express.static(__dirname + "/join_us_client/build/"));
+app.get(/\/join_us\/*/, insertData);
+app.get("/hello", getData);
+
+app.listen(port, () =>
+  console.log("Express server is running on localhost:", port)
+);
